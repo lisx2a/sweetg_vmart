ce613473d26ec41ddb7df6f82acf2c80f1a1a71b
Lissa <lissa.aguilar@gmail.com>
2011-08-15 01:32:58 -0400

    adding vmart joomla files from local development environment

---
+++ logs/error.php
@@ -0,0 +1,57 @@
+#<?php die('Direct Access To Log Files Not Permitted'); ?>
+#Version: 1.0
+#Date: 2010-08-23 14:07:01
+#Fields: date	time	level	c-ip	status	comment
+#Software: Joomla! 1.5.20 Stable [ senu takaa [VirtueMart eCommerce Bundle] ] 18-July-2010 18:00 GMT
+2010-08-23	14:07:01	-	::1	-	Found User
+2010-08-23	14:07:01	-	::1	-	No user found with Payer ID in Virtuemart
+2010-08-23	14:50:53	-	::1	-	Found User
+2010-08-23	14:50:53	-	::1	-	No user found with Payer ID in Virtuemart
+2010-08-23	14:57:23	-	::1	-	Found User
+2010-08-23	14:57:23	-	::1	-	No user found with Payer ID in Virtuemart
+2011-02-25	17:53:23	-	::1	-	Found User
+2011-02-25	17:53:23	-	::1	-	No user found with Payer ID in Virtuemart
+2011-08-08	04:28:26	-	127.0.0.1	-	Found User
+2011-08-08	04:28:26	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-08	06:25:19	-	127.0.0.1	-	Found User
+2011-08-08	06:25:19	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-08	06:25:34	-	127.0.0.1	-	Found User
+2011-08-08	06:25:34	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-09	01:11:43	-	127.0.0.1	-	Found User
+2011-08-09	01:11:43	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-09	01:11:52	-	127.0.0.1	-	Found User
+2011-08-09	01:11:52	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-09	01:12:06	-	127.0.0.1	-	Found User
+2011-08-09	01:12:06	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-09	02:10:50	-	127.0.0.1	-	Found User
+2011-08-09	02:10:50	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-09	03:20:36	-	127.0.0.1	-	Found User
+2011-08-09	03:20:36	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-09	03:38:02	-	127.0.0.1	-	Found User
+2011-08-09	03:38:02	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	18:30:47	-	127.0.0.1	-	Found User
+2011-08-13	18:30:47	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	18:31:04	-	127.0.0.1	-	Found User
+2011-08-13	18:31:04	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	18:31:14	-	127.0.0.1	-	Found User
+2011-08-13	18:31:14	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	18:32:14	-	127.0.0.1	-	User does not exist
+2011-08-13	18:32:47	-	127.0.0.1	-	User does not exist
+2011-08-13	18:32:59	-	127.0.0.1	-	Found User
+2011-08-13	18:32:59	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	18:33:07	-	127.0.0.1	-	Found User
+2011-08-13	18:33:07	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	18:33:28	-	127.0.0.1	-	Found User
+2011-08-13	18:33:28	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	18:34:18	-	127.0.0.1	-	Found User
+2011-08-13	18:34:18	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	18:34:26	-	127.0.0.1	-	Found User
+2011-08-13	18:34:26	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	20:39:15	-	127.0.0.1	-	Found User
+2011-08-13	20:39:15	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	20:39:27	-	127.0.0.1	-	Found User
+2011-08-13	20:39:27	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	20:39:48	-	127.0.0.1	-	Found User
+2011-08-13	20:39:48	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
+2011-08-13	20:40:08	-	127.0.0.1	-	Found User
+2011-08-13	20:40:08	-	127.0.0.1	-	No user found with Payer ID in Virtuemart
